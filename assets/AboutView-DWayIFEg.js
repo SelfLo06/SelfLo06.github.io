82b03c1c6b7f88986742c9e06f10f209a89926fe
SelfLo06 <SelfLo06@users.noreply.github.com>
2025-07-12 05:16:01 +0000

    deploy: 92a135d40e55dcb82d18b47d6b6f3c41107dde70

--- assets/AboutView-DWayIFEg.js
+++ assets/AboutView-DWayIFEg.js
@@ -1,5 +1,5 @@
-import{k as a,P as s}from"./PageHeader-B5QnN3d0.js";import{_ as n,r,m as c,c as i,f as l,j as e,F as _,h as d}from"./index-D7y8XOpk.js";const u={class:"about-view card"},m=["innerHTML"],p={__name:"AboutView",setup(f){const t=r(`
-Hiii, 我是 selflo，一名来自**南京大学智能软件**的大一学生！ (^▽^)
+import{k as a,P as s}from"./PageHeader-BbTQNiLp.js";import{_ as n,r,m as c,c as i,f as l,j as e,F as d,h as _}from"./index-DJKjWz8k.js";const u={class:"about-view card"},m=["innerHTML"],p={__name:"AboutView",setup(f){const t=r(`
+Hiii, 我是 selflo，一名来自**南京大学智能软件**的学生！ (^▽^)
 
 这个博客诞生于我的大一暑假（2025.7.3 - 2025.7.8），是我给自己布置的一份复习和巩固知识的暑期作业，也是一个**从零开始，完全手写**~(存疑)~的项目。
 
@@ -10,4 +10,4 @@ Hiii, 我是 selflo，一名来自**南京大学智能软件**的大一学生！
 我也不知道这个博客能更新多久~（笑，明明刚创建就说这种话）~，但真心希望它能记录下我的成长轨迹。
 
 总之，非常感谢你愿意点开我的博客来看！ ヾ(o´▽\`o)ﾉ
-`),o=c(()=>a(t.value));return(v,b)=>(d(),i(_,null,[l(s,{title:"关于我","cover-image":"https://cdn.jsdelivr.net/gh/SelfLo06/MyImages@main/background.jpg"}),e("div",u,[e("div",{class:"content",innerHTML:o.value},null,8,m)])],64))}},H=n(p,[["__scopeId","data-v-f03c397b"]]);export{H as default};
+`),o=c(()=>a(t.value));return(v,g)=>(_(),i(d,null,[l(s,{title:"关于我","cover-image":"https://cdn.jsdelivr.net/gh/SelfLo06/MyImages@main/background.jpg"}),e("div",u,[e("div",{class:"content",innerHTML:o.value},null,8,m)])],64))}},H=n(p,[["__scopeId","data-v-721d3292"]]);export{H as default};
