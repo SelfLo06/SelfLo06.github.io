74027436729be9bbc0c101362bf195d75d9fdaa4
SelfLo06 <SelfLo06@users.noreply.github.com>
2025-08-13 12:31:39 +0000

    deploy: 28b54e769fcf9d0e338d374f8a2f0f7254ffb078

---
+++ assets/AboutView-CUZk0cXw.js
@@ -0,0 +1,13 @@
+import{M as i,i as u,m as d,H as s,P as m}from"./index-41WDKi-Y.js";import{_ as h,l as g,r as p,m as _,c as f,f as v,j as r,n as H,u as b,F as k,h as w}from"./index-cqugP81o.js";import"./_commonjsHelpers-CqkleIqs.js";const C={class:"about-view card"},M=["innerHTML"],j={__name:"AboutView",setup(x){const o=g(),e=new i({html:!0,linkify:!0,breaks:!0,highlight:function(a,t){if(t&&s.getLanguage(t))try{return'<pre class="hljs"><code>'+s.highlight(a,{language:t,ignoreIllegals:!0}).value+"</code></pre>"}catch(l){console.warn("代码高亮失败:",l)}return'<pre class="hljs"><code>'+e.utils.escapeHtml(a)+"</code></pre>"}});e.use(u),e.use(d);const n=p(`
+Hiii, 我是 selflo，一名来自**南京大学智能软件**的学生！ (^▽^)
+
+这个博客诞生于我的大一暑假（2025.7.3 - 2025.7.8），是我给自己布置的一份复习和巩固知识的暑期作业，也是一个**从零开始，完全手写**(存疑)的项目。
+
+在UI设计上，很大程度上参考了优秀的 Hexo 主题，并在此基础上进行了简化和二次创作。
+
+说实话，建立这个博客的过程，大概是 90% 的 \`Ctrl+C\` + \`Ctrl+V\`，加上 10% 的灵感……而这 10% 的灵感，几乎全部来自于我强大的开发伙伴——**伟大的 Gemini**！在它的辅助下，整个开发过程充满了探索的乐趣。
+
+我也不知道这个博客能更新多久~（笑，明明刚创建就说这种话）~，但真心希望它能记录下我的成长轨迹。
+
+==总之，非常感谢你愿意点开我的博客来看！== ヾ(o´▽\`o)ﾉ
+`),c=_(()=>e.render(n.value));return(a,t)=>(w(),f(k,null,[v(m,{title:"关于我","cover-image":"https://cdn.jsdelivr.net/gh/SelfLo06/MyImages@main/background.jpg"}),r("div",C,[r("div",{class:H(["content",{"dark-theme":b(o).theme==="dark"}]),innerHTML:c.value},null,10,M)])],64))}},S=h(j,[["__scopeId","data-v-d7be62f9"]]);export{S as default};
